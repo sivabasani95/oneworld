81ca8c8739c006e63779fc07b04a52d1288a1d25
sivabasani95 <sivabasani95@gmail.com>
2025-11-24 13:39:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ function changeMessage() {
   const msg = document.getElementById("message");
   msg.textContent = "You clicked the button!";
 }
-// just trying in feature-branch
-console.log("you clicked the button!");
+
