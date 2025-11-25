08f66238cffd530b1555fb06ad1a9ed49168a3ea
Abhir <sivabasani95@gmail.com>
2025-11-24 18:36:55 -0600

    Save changes in script.js before pulling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function changeMessage() {
-  const msg = document.getElementById("message");
-  msg.textContent = "You clicked the button!";
-}
+console.log("Start");
+for (let i = 0; i < 3; i++) {
+    console.log("Iteration", i);
+}console.log("End");
